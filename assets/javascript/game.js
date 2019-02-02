9c9caf772f624d5e82aa143662af14b5a432ee54
DavidFarnick <dfarnick2@gmail.com>
2019-02-01 20:54:41 -0600

    final final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
-//selects random number at start of game. 9-49
+//selects random number at start of game. 19-120
 $(document).ready(function(){
-    var randomValue=Math.floor(Math.random()*40+9);
+    var randomValue=Math.floor(Math.random()*101+19);
 //appending random number to randomValue
-$("#targetNumber").html(randomValue);
+    $("#targetNumber").html(randomValue);
 
-//values for skulls
-let val1=Math.floor(Math.random()*10+2);
-let val2=Math.floor(Math.random()*10+2);
-let val3=Math.floor(Math.random()*10+2);
-let val4=Math.floor(Math.random()*10+2);
+//values for skulls 1-12
+let val1=Math.floor(Math.random()*11+1);
+let val2=Math.floor(Math.random()*11+1);
+let val3=Math.floor(Math.random()*11+1);
+let val4=Math.floor(Math.random()*11+1);
 
 let valueTotal = 0;
 let wins = 0;
@@ -19,73 +19,70 @@ $("#skullOne").on ("click", function(){
     console.log("New Total: " + valueTotal);
     $("#total").html(valueTotal); 
           
-        if (valueTotal == randomValue){
-          victor();
-        }
-        else if ( valueTotal > randomValue){
-          loser();
-        }   
+    if (valueTotal == randomValue){
+        victor();
+    }
+    else if ( valueTotal > randomValue){
+        loser();
+    }   
   })  
-  $("#skullTwo").on ("click", function(){
+$("#skullTwo").on ("click", function(){
     valueTotal = valueTotal + val2;
     $("#total").html(valueTotal); 
           
-        if (valueTotal == randomValue){
-          victor();
-        }
-        else if ( valueTotal > randomValue){
-          loser();
-        }   
+    if (valueTotal == randomValue){
+        victor();
+    }
+    else if ( valueTotal > randomValue){
+        loser();
+    }   
   })  
-  $("#skullThree").on ("click", function(){
+$("#skullThree").on ("click", function(){
     valueTotal = valueTotal + val3;
     $("#total").html(valueTotal); 
           
-        if (valueTotal == randomValue){
-          victor();
-        }
-        else if ( valueTotal > randomValue){
-          loser();
-        }   
+    if (valueTotal == randomValue){
+        victor();
+    }
+    else if ( valueTotal > randomValue){
+        loser();
+    }   
   })  
-  $("#skullFour").on ("click", function(){
+$("#skullFour").on ("click", function(){
     valueTotal = valueTotal + val4;
     $("#total").html(valueTotal); 
          
-        if (valueTotal == randomValue){
-          victor();
-        }
-        else if ( valueTotal > randomValue){
-          loser();
-        }   
+    if (valueTotal == randomValue){
+        victor();
+    }
+    else if ( valueTotal > randomValue){
+        loser();
+    }   
   })
     
 function resetGame(){
-    randomValue=Math.floor(Math.random()*40+9);
+    randomValue=Math.floor(Math.random()*101+19);
     console.log("reset is working: ", randomValue)
     $('#targetNumber').html(randomValue);
-    val1= Math.floor(Math.random()*10+2);
-    val2= Math.floor(Math.random()*10+2);
-    val3= Math.floor(Math.random()*10+2);
-    val4= Math.floor(Math.random()*10+2);
+    val1= Math.floor(Math.random()*11+1);
+    val2= Math.floor(Math.random()*11+1);
+    val3= Math.floor(Math.random()*11+1);
+    val4= Math.floor(Math.random()*11+1);
     valueTotal= 0;
     $('#total').html(valueTotal);
     } 
     
     $("#wins").html(wins);
     $("#losses").html(losses);
-function victor(){
-    if (confirm("God Damn, Frankie. I LOVE TO WATCH YOU WORK.")){
-      txt = "They JUST KEEP COMING!"
-        wins++; 
-      $('#wins').html(wins);
-      resetGame();
+
+///I attempted to get a confirm to work here just for fun but...
+///couldn't get it to work the way I wanted to so settled with an alert.
+    function victor(){
+    alert("God Damn, Frankie. I love to watch you work.")
+    wins++; 
+    $('#wins').html(wins);
+    resetGame();   
     }
-    else {
-        txt= "You need more training!"
-    }
-    }
-    
 function loser(){    
       losses++;
       $('#losses').html(losses);
